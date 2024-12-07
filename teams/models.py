9d22f7b1887d835a84646c6af6a74e1709606423
Soham <141822047+XohamOG@users.noreply.github.com>
2024-12-07 20:01:19 +0530

    updated backend databse model to suit with frontend data

--- teams/models.py
+++ teams/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Player(models.Model):
+    POSITION_CHOICES = [
+        ('Goalkeeper', 'Goalkeeper'),
+        ('Attacker', 'Attacker'),
+        ('Defender', 'Defender'),
+        ('Player', 'Player'),
+    ]
+    
     name = models.CharField(max_length=100)
     rating = models.IntegerField()
-    position = models.CharField(max_length=50, choices=[('Goalkeeper', 'Goalkeeper'), ('Player', 'Player')], default='Player')
+    position = models.CharField(max_length=50, choices=POSITION_CHOICES, default='Player')
     is_selected = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
