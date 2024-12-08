0093349c86a280bb51bcf0889700c4a0a7a266b2
Soham <141822047+XohamOG@users.noreply.github.com>
2024-12-08 21:12:10 +0530

    settings.py updated

--- TeamDivider/settings.py
+++ TeamDivider/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a2q8c#uluw6p#35#ymn3hs)polq@@=fcxa+$n@tcd_mjrx3s^+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['TeamDivider.onrender.com']
+ALLOWED_HOSTS = ['https://teamdivider.onrender.com']
 
 # Application definition
 
